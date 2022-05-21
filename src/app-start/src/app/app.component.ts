7e8f93cd0fe981f0a3fec8e1f5ba018bc25cb8fc
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-21 10:53:43 +0430

    EventBinding

--- src/app-start/src/app/app.component.ts
+++ src/app-start/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-start';
+  onButtonClick(){
+   console.log('button click');
+  }
 }
