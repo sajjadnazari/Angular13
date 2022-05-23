f9db369c3edcacebec65b6014311426157d62273
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-23 11:14:00 +0430

    property binding and ngFor directive

--- src/app-start/src/app/app.component.ts
+++ src/app-start/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title ="app-start";
+  posts =[
+    {
+      title :"one",
+      imageUrl:"assets/images/1.png",
+      username:"jffjsd",
+      content:"hello word"
+    },
+    {
+      title :"two",
+      imageUrl:"assets/images/2.png",
+      username:"john",
+      content:"hello word"
+    },
+    {
+      title :"three",
+      imageUrl:"assets/images/3.png",
+      username:"mike",
+      content:"hello word"
+    }
+  ]
 
+  getPosts( )
+  { return this.posts}
 }
