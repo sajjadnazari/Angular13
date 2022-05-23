9509048904b4d00dd9e9bf40f72b49480434e161
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-23 10:05:06 +0430

    add bulma package and style and add new card component

--- src/app-start/src/app/app.component.ts
+++ src/app-start/src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  password:string;
-  constructor() {
-    this.password="";
-  }
-  onButtonClick() {
-    this.password = "your pass";
-  }
-  getPassword(){
-    return this.password;
-  }
-  ChangeCheckBox(){
-    this.password ="changed";
-  }
 
-  OnChangeLentgh(value :any){
-    console.log(value);
-  }
 }
