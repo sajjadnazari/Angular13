0cee2d770fa210f69eb4f0631266dceff99b0d9e
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-24 10:31:52 +0430

    install faker 5.5.3

--- src/app-start/src/app/app.component.ts
+++ src/app-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { lorem } from "faker";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,27 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title ="app-start";
-  posts =[
-    {
-      title :"one",
-      imageUrl:"assets/images/1.png",
-      username:"jffjsd",
-      content:"hello word"
-    },
-    {
-      title :"two",
-      imageUrl:"assets/images/2.png",
-      username:"john",
-      content:"hello word"
-    },
-    {
-      title :"three",
-      imageUrl:"assets/images/3.png",
-      username:"mike",
-      content:"hello word"
-    }
-  ]
-
-  getPosts( )
-  { return this.posts}
+  randomText :string = lorem.sentence();
 }
