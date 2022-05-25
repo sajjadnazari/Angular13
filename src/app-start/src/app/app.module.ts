5c18f600d95d600a22c1671145a2c63b8f3e3206
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-25 10:31:04 +0430

    Pipe in angular

--- src/app-start/src/app/app.module.ts
+++ src/app-start/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ConvertPipe } from './convert.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ConvertPipe
   ],
   imports: [
     BrowserModule,
