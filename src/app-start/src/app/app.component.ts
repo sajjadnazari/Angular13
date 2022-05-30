17c253316c135ca7bdcbdc2313c56cbcb40e14e7
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-05-30 13:35:23 +0430

    remove components . for intialize Module learning

--- src/app-start/src/app/app.component.ts
+++ src/app-start/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { lorem } from "faker";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,33 +6,5 @@ import { lorem } from "faker";
 })
 export class AppComponent {
   title ="app-start";
-  isExist :boolean =false;
-  currentPage : number = 0;
-  setActiveClass(index:number){
-    return index ===this.currentPage?'active':'';
-  }
-  checkWindowIndex(index : number){
-    return Math.abs(this.currentPage - index) < 5
-  }
-  changePage(){
-    this.currentPage -=1;
-  }
-  images = [
-    {
-      title:'test1',
-      url:'https://ps.w.org/woo-free-product-sample/assets/icon-256x256.png?rev=2279441'
-    },
-    {
-      title:'test2',
-      url:'https://ps.w.org/woo-free-product-sample/assets/icon-256x256.png?rev=2279441'
-    },
-    {
-      title:'test3',
-      url:'https://ps.w.org/woo-free-product-sample/assets/icon-256x256.png?rev=2279441'
-    },
-    {
-      title:'test4',
-      url:'https://ps.w.org/woo-free-product-sample/assets/icon-256x256.png?rev=2279441'
-    }
-]
+
 }
--- src/app-start/src/app/app.module.ts
+++ src/app-start/src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { ConvertPipe } from './convert.pipe';
-import { SampleDirective } from './sample.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CardComponent,
-    ConvertPipe,
-    SampleDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
