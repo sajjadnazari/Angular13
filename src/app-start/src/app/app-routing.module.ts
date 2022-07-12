43d070c105a5977ada1ef578be5cf9444e5b496d
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-07-12 19:52:33 +0430

    not found routing

--- src/app-start/src/app/app-routing.module.ts
+++ src/app-start/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-
+ {path :'' ,component : HomeComponent},
+ {path :'**' ,component: NotFoundComponent}
 ];
 
 @NgModule({
